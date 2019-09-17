c8c8d7d762b3ee797526d67f5bd2e491f57ea3f8
Damien Asseya <damien.asseya@caci.com>
2019-09-17 16:02:32 -0400

    Merge sort

--- main.js
+++ main.js
@@ -140,7 +140,8 @@
 				break;
 			case "merge":
 				console.profile("mergeSort");
-				mergeSort();
+				const mergeArr = mergeSort(itemsNotNull);
+				renderNodes(mergeArr);
 				console.profileEnd("mergeSort");
 				break;
 			case "insertion":
@@ -194,11 +195,39 @@
 		} while(swapped);
 	}
 	function mergeSort (arr) {
-		
+		if(arr.length <= 1) {
+			return arr;
+		}
+		const middle = Math.floor(arr.length / 2), 
+				left = arr.slice(0, middle), 
+			   right = arr.slice(middle);
+		return merge(
+			mergeSort(left),
+			mergeSort(right)
+		)
 	}
 	
 	function merge (left, right) {
-		
+		let results = []
+		indexLeft = 0,
+		indexRight = 0;
+		const sorterIndex = headers.indexOf(sorters[0]);
+		while(indexLeft < left.length & indexRight < right.length) {
+			const rowA = Array.from(left[indexLeft].childNodes);
+			const rowB = Array.from(right[indexRight].childNodes);
+			const x = parseFloat(rowA[sorterIndex].textContent)
+			const y = parseFloat(rowB[sorterIndex].textContent)
+			if(x < y) {
+				results.push(left[indexLeft]);
+				indexLeft++;
+			} else {
+				results.push(right[indexRight]);
+				indexRight++;
+			}
+			return results.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+		}
+
+
 	}
 	function insertionSort (arr) {
 		
